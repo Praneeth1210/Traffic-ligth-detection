1d68600fb1bd3edfe86078101aefc4825b17b216
Praneeth1210 <83580844+Praneeth1210@users.noreply.github.com>
2022-07-17 11:15:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:'https://www.nyckel.com/v1/functions/pxvrvn451z7vtd44/invoke',
+        url:'https://www.nyckel.com/v1/functions/7nes80bn4i056l3w/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
